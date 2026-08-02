b17c299f113fd87ba58d17898dd612d89b350086
Michael Sault <michael_sault@hotmail.com>
2026-08-02 00:54:08 -0400

    updated the commands to fetch only specific videos

--- index.js
+++ index.js
@@ -1,54 +1,23 @@
 import { ChannelType, Client, Events, SlashCommandBuilder } from 'discord.js';
 import { joinVoiceChannel, VoiceConnection } from '@discordjs/voice';
 
-import { downloadNew, downloadVideo } from './youtubedl.js';
+import { downloadNew, downloadVideo, generateNFO } from './youtubedl.js';
+import { channels } from './channels.js';
 
 import * as dotenv from 'dotenv';
 dotenv.config();
 const TOKEN = process.env.DISCORD_TOKEN;
-
-console.log( TOKEN );
+const YT_DELAY = 90_000; //waits 90 seconds
+const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 const client = new Client({intents: []});
 
-client.once(Events.ClientReady, c => {
-    console.log(`Logged in as ${c.user.username}`);
 
-    const ping = new SlashCommandBuilder()
-        .setName("waddleping")
-        .setDescription("Replies with pong!");
-
-    const join = new SlashCommandBuilder()
-        .setName('waddlejoin')
-        .setDescription('Joins a Voice Channel')
-        .addChannelOption((option) => 
-            option.setName('channel')
-            .setDescription('The channel to join')
-            .setRequired(true)
-            .addChannelTypes(ChannelType.GuildVoice)
-        )
-        .toJSON();
-
-    const waddleNew = new SlashCommandBuilder()
-        .setName('waddleupdate')
-        .setDescription('Updates followed youtube channels!');
-
-    const waddleVideo = new SlashCommandBuilder()
-        .setName('waddlevideo')
-        .setDescription('Downloads the linked youtube video!')
-        .addChannelOption((option) =>
-            option.setName('url')
-            .setDescription('The URL of the video to download')
-            .setRequired(true)
-        )
-        .toJSON();
-});
-
-client.on(Events.InteractionCreate, interaction => {
+client.on(Events.InteractionCreate, async interaction => {
     if (interaction.isChatInputCommand()) {
 
-        if(interaction.commandName === "waddleupdate"){
-            interaction.reply("Waddle Pong!");
+        if(interaction.commandName === "waddleping"){
+            interaction.reply('Waddle Pong!');
         }
 
         else if(interaction.commandName === "waddlejoin"){
@@ -60,20 +29,32 @@ client.on(Events.InteractionCreate, interaction => {
             });
         }
 
-        else if(interaction.commandName === "waddleping"){
+        else if(interaction.commandName === "waddleupdate"){
             interaction.reply("WaddleB is grabbing new content")
-            const favorites = ['KeltieOConnor', 'graciekramer14', 'makariespe', /**'thegrumps',**/ 'CurrentlyHannah', /**'GameGrumps',**/ 'PhilyBowden', 'rileyrehl', 'bugfishhhh', 'EthosLab'];
-            const folders = ["Keltie O'Connor", 'Gracie Kramer', 'Makari Espe', /**'The Grumps',**/ 'Currently Hannah', /**'GameGrumps',**/ 'Phily Bowden', 'Riley Rehl', 'bugfishhhh', 'EthosLab'];
             
-            for (var i=0; i< favorites.length; i++) {
-                downloadNew("graciekramer14", "Gracie Kramer");
-            };
-            
+            const selectedChannel = interaction.options.getString('channel');
+
+            if (selectedChannel == null) {
+                for (let i = 0; i < channels.length; i++) {
+                    const channel = channels[i];
+                    await downloadNew(channel.channel, channel.folder);
+
+                    if (i < channels.length - 1) {
+                        await delay(YT_DELAY);
+                    }
+                }
+            } else {
+                const channel = channels.find(
+                    channel => channel.option === selectedChannel
+                );
+
+                await downloadNew(channel.channel, channel.folder);
+            }
         }
 
-        else if(interaction.commandName === "ping"){
-            const url = 'https://www.youtube.com/watch?v=tollGa3S0o8';
-            interaction.reply("WaddleB is fetching", url);
+        else if(interaction.commandName === "waddlevideo"){
+            const url = interaction.options.getString('url', true);
+            interaction.reply(`WaddleB is fetching: ${url}`);
             downloadVideo(url);
         }
     }
