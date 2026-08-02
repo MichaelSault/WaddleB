6d7d2c409d92d7299c1768f869d8f4614abf2333
Michael Sault <michael_sault@hotmail.com>
2026-08-01 23:59:12 -0400

    updated commands to only pull specified videos

---
+++ channels.js
@@ -0,0 +1,12 @@
+export const channels = [
+    { option: 'keltie', channel: 'KeltieOConnor', folder: "Keltie O'Connor" },
+    { option: 'makari', channel: 'makariespe', folder: 'Makari Espe' },
+    { option: 'grumps', channel: 'thegrumps', folder: 'The Grumps' },
+    { option: 'hannah', channel: 'CurrentlyHannah', folder: 'Currently Hannah' },
+    { option: 'gamegrumps', channel: 'GameGrumps', folder: 'GameGrumps' },
+    { option: 'phily', channel: 'PhilyBowden', folder: 'Phily Bowden' },
+    { option: 'riley', channel: 'rileyrehl', folder: 'Riley Rehl' },
+    { option: 'bugfish', channel: 'bugfishhhh', folder: 'bugfishhhh' },
+    { option: 'ethoslab', channel: 'EthosLab', folder: 'EthosLab' },
+    { option: 'gracie', channel: 'graciekramer14', folder: 'Gracie Kramer' },
+];
